fc234e0a9d1d2f21ddd7f2f15c1ebfe4d4da7b9c
EugeniBykovski <bykovskieugeni@mail.ru>
2023-07-20 15:08:50 +0200

    added auth

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
-  return <p>Dashboard Page</p>;
+  return (
+    <div>
+      <p>Dashboard Page</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default DashboardPage;
